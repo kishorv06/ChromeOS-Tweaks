98f0abc28eed1429e79fe1dee621fbcbcde5fa89
Kishor V <developer.kishor@gmail.com>
2018-11-25 00:11:16 +0530

    Update install.sh

--- MountFS/install.sh
+++ MountFS/install.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-if ! [[ $EUID = 0 ]]; then
-  echo "Please run as Root"
-  exit
-fi
-
 cd /bin/
-curl -Ls https://raw.githubusercontent.com/kishorv06/ChromeOS-Tweaks/master/MountFS/mount-internals
-chmod a+x mount-internals
+sudo curl -Ls https://raw.githubusercontent.com/kishorv06/ChromeOS-Tweaks/master/MountFS/mount-internals
+sudo chmod a+x mount-internals
 
 cd /etc/init/
-curl -Ls https://raw.githubusercontent.com/kishorv06/ChromeOS-Tweaks/master/MountFS/mount-internals.conf
+sudo curl -Ls https://raw.githubusercontent.com/kishorv06/ChromeOS-Tweaks/master/MountFS/mount-internals.conf
 
-/bin/mount-internals
+sudo /bin/mount-internals
 
 echo "Installed Successfully"
